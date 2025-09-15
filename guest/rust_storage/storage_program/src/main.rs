f705e3bac8a1c581aeb26315c0dad3a229c835ea
Laurence Rowe <l@lrowe.co.uk>
2025-09-14 20:26:07 -0700

    >>> Guest: target/x86_64-unknown-linux-gnu/release/demo  >>> Storage: storage_program/target/x86_64-unknown-linux-musl/release/demo Hello, Storage World! Called from thread: 1 Unhandled system call: 2 terminate called after throwing an instance of 'tinykvm::MemoryException'   what():  page_at: page directory not present Aborted (core dumped)

--- guest/rust_storage/storage_program/src/main.rs
+++ guest/rust_storage/storage_program/src/main.rs
@@ -1,22 +1,15 @@
 #![feature(thread_id_value)]
-use std::cell::RefCell;
 use std::process::ExitCode;
 
-thread_local! {
-  pub static COUNT: std::cell::RefCell<i32> = const { RefCell::new(0) };
-  pub static VALUES: std::cell::RefCell<Vec<i32>> = const { RefCell::new(Vec::new()) };
-}
-
 #[no_mangle]
-extern "C" fn remote_function(arg: extern "C" fn(i32) -> i32, value: i32) -> i32 {
+extern "C" fn remote_function(_arg: extern "C" fn(i32) -> i32, value: i32) -> i32 {
     println!(
         "Called from thread: {}",
         std::thread::current().id().as_u64()
     );
-    COUNT.with_borrow_mut(|count| *count += 1);
-    VALUES.with_borrow_mut(|v| v.push(value)); // XXX This line makes it crash
-    println!("After push");
-    return arg(value);
+		let conn = rusqlite::Connection::open_in_memory().unwrap();
+		let mut stmt = conn.prepare("SELECT 1 + ?1").unwrap();
+		stmt.query_one([value], |row| row.get(0)).unwrap()
 }
 
 extern "C" fn double_int(input: i32) -> i32 {
