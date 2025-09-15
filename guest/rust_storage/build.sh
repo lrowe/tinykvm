38db62acde0bfbb4424b8265859b0b85c1df86db
Laurence Rowe <l@lrowe.co.uk>
2025-09-14 19:28:33 -0700

    Error allocating space for vector
    >>> Guest: target/x86_64-unknown-linux-gnu/release/demo  >>> Storage: storage_program/target/x86_64-unknown-linux-musl/release/demo
    Hello, Storage World!
    Called from thread: 1
    After push
    Hello, world!
    *** Page Fault on address 0x1
    Error code: 0x1 (memory read)
    * Protection violation
    CR0: 0x80050033  CR3: 0x9000
    CR2: 0x1  CR4: 0x350620
    RAX: 0x1  RBX: 0x4B8D68  RCX: 0x0
    RDX: 0xFFFFFFFFFFFFFFFC  RSI: 0x15  RDI: 0x1
    RIP: 0x2304  RBP: 0xD  RSP: 0x3FC0
    SS: 0x0  CS: 0x8  DS: 0x23  FS: 0x0  GS: 0x0
    FS BASE: 0x441F6090  GS BASE: 0x5050
    Failing RIP: 0x6
    Fail RFLAGS: 0x2B
    Failing CS:  0x44008780
    Failing RSP: 0x13002
    Failing SS:  0x4B8AD8
    RIP  0x6   (error: rip < image base)
    Exception: Kernel or zero page fault with data 0xE
    CR0: 0x80050033  CR3: 0x9000
    CR2: 0x1  CR4: 0x350620
    RAX: 0x1  RBX: 0x4B8D68  RCX: 0x0
    RDX: 0xFFFFFFFFFFFFFFFC  RSI: 0x15  RDI: 0x1
    RIP: 0x2304  RBP: 0xD  RSP: 0x3FC0
    SS: 0x0  CS: 0x8  DS: 0x23  FS: 0x0  GS: 0x0
    FS BASE: 0x441F6090  GS BASE: 0x5050
    Error: Failed to initialize main VM, exiting

--- guest/rust_storage/build.sh
+++ guest/rust_storage/build.sh
@@ -5,7 +5,7 @@ pushd storage_program
 popd
 storage_binary=./storage_program/target/x86_64-unknown-linux-musl/release/demo
 objcopy -w --extract-symbol --strip-symbol=!remote* --strip-symbol=* $storage_binary storage.syms
-gcc-12 -static -O2 symbol_offset.c -o symbol_offset
+gcc -static -O2 symbol_offset.c -o symbol_offset
 ./symbol_offset storage.syms 0x44000000
 
 RUSTFLAGS="-Ctarget-feature=+crt-static -Zexport-executable-symbols -C link_arg=-Wl,--just-symbols=storage.syms" cargo build --target x86_64-unknown-linux-gnu --release
--- guest/rust_storage/storage_program/src/main.rs
+++ guest/rust_storage/storage_program/src/main.rs
@@ -1,12 +1,30 @@
+#![feature(thread_id_value)]
+use std::cell::RefCell;
 use std::process::ExitCode;
 
-#[no_mangle]
-extern "C" fn remote_function(arg: fn(i32) -> i32, value: i32) -> i32 {
-	return arg(value);
+thread_local! {
+  pub static COUNT: std::cell::RefCell<i32> = const { RefCell::new(0) };
+  pub static VALUES: std::cell::RefCell<Vec<i32>> = const { RefCell::new(Vec::new()) };
 }
 
-fn main() -> ExitCode
-{
-	println!("Hello, Storage World!");
-	return ExitCode::from(123);
+#[no_mangle]
+extern "C" fn remote_function(arg: extern "C" fn(i32) -> i32, value: i32) -> i32 {
+    println!(
+        "Called from thread: {}",
+        std::thread::current().id().as_u64()
+    );
+    COUNT.with_borrow_mut(|count| *count += 1);
+    VALUES.with_borrow_mut(|v| v.push(value)); // XXX This line makes it crash
+    println!("After push");
+    return arg(value);
+}
+
+extern "C" fn double_int(input: i32) -> i32 {
+    return input * 2;
+}
+
+fn main() -> ExitCode {
+    println!("Hello, Storage World!");
+    remote_function(double_int, 1);
+    return ExitCode::from(123);
 }
