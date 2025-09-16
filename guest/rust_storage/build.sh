7f999a935730125da238d51aa01152885e86caaa
Laurence Rowe <l@lrowe.co.uk>
2025-09-16 10:11:34 -0700

    Combine rust_storage main and storage programs into single crate

--- guest/rust_storage/build.sh
+++ guest/rust_storage/build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
-pushd storage_program
-./build.sh
-popd
-storage_binary=./storage_program/target/release/demo
+CARGO_TARGET_DIR=./target-storage RUSTFLAGS="-Ctarget-feature=+crt-static -Zexport-executable-symbols" cargo build --release --bin storage
+#RUSTFLAGS="-Zexport-executable-symbols" cargo build --target x86_64-unknown-linux-musl --release --bin storage
+storage_binary=./target-storage/release/storage
 objcopy -w --extract-symbol --strip-symbol=!remote* --strip-symbol=* $storage_binary storage.syms
-gcc-12 -static -O2 symbol_offset.c -o symbol_offset
+gcc -static -O2 symbol_offset.c -o symbol_offset
 ./symbol_offset storage.syms 0x44000000
 
 rm -rf target/
-RUSTFLAGS="-Zexport-executable-symbols -C link_arg=-Wl,--undefined=do_calculation,--just-symbols=$PWD/storage.syms" cargo build --release
+RUSTFLAGS="-Zexport-executable-symbols -C link_arg=-Wl,--undefined=do_calculation,--just-symbols=$PWD/storage.syms" cargo build --release --bin main
--- guest/rust_storage/storage_program/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-RUSTFLAGS="-Ctarget-feature=+crt-static -Zexport-executable-symbols" cargo build --release
-#RUSTFLAGS="-Zexport-executable-symbols" cargo build --target x86_64-unknown-linux-musl --release
